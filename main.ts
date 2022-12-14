046eb2b0ca697c397ea05bec9c959e5dc977c66d
Pingu-is-hot <120554821+Pingu-is-hot@users.noreply.github.com>
2022-12-14 09:14:00 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,59 @@
+basic.forever(function () {
+    music.playMelody("C A F E D F G A ", 120)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # . # . #
+        # . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        # # . . .
+        . . # . #
+        # # . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        # # # . .
+        # # # # .
+        # # . . #
+        # # # # .
+        # # # . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . # # #
+        . # # # #
+        . # # # #
+        . # # # #
+        . . # # #
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . # # #
+        . . # # #
+        . . # # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . . #
+        `)
+})
